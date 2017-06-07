be6ee0206294b60fe362c60068f2c866cde2748c
kevinmeder <kevin.p.meder@gmail.com>
2017-06-06 21:53:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     
-    var windowWidth = 0,
+    let windowWidth = 0,
         columnCount,
         containerWidth,
         columns = [],
@@ -8,18 +8,20 @@ $(function(){
         startPhase = true;
 
     // adjust width of columns and gutters
-    var columnWidth = 400,
+    let columnWidth = 400,
         gutter = 30;
 
-    $(window).load(resize);
+    $(window).on('load', resize);
     $(window).resize(resize);
 
     function resize(phase){
-
+        const $pins = $('ul.pins');
+        const $pin = $('li.pin');
+        
         // set column and gutter css
-        if(startPhase === true){
-            $("ul.pins").css({ "min-width": columnWidth + "px" });
-            $("ul.pins li").css({ "width": (columnWidth-gutter) + "px", "padding": "0px " + (gutter/2) + "px" });
+        if(startPhase){
+            $pins.css({ 'min-width': `${columnWidth}px` });
+            $pin.css({ 'width': (columnWidth - gutter) + 'px', 'padding': '0px ' + (gutter / 2) + 'px' });
             startPhase = false;
         }
 
@@ -27,9 +29,9 @@ $(function(){
         if(windowWidth !== $(window).width()){
             windowWidth = $(window).width();
             columnCount = Math.floor(windowWidth/columnWidth);
-            columnCount = (columnCount === 0) ? 1 : columnCount ;    
-            containerWidth = columnCount*columnWidth;
-            $("ul.pins").css({"width": containerWidth + "px"});
+            columnCount = (columnCount === 0) ? 1 : columnCount;    
+            containerWidth = columnCount * columnWidth;
+            $pins.css({ 'width': `${containerWidth}px` });
             
             // clear column array for window resize
             columns = [];
@@ -38,25 +40,25 @@ $(function(){
                 columns[i] = 0; 
             }
 
-            $("ul.pins li").each(function(){
+            $pin.each(function(){
                 var pinHeight = $(this).height();
                 var minColumnHeight = Math.min.apply(Math,columns);
                 var minColumnKey = $.inArray( minColumnHeight, columns );
 
-                $(this).css({"top": minColumnHeight + "px", "left": (columnWidth*minColumnKey) + "px"});
+                $(this).css({ 'top': `${minColumnHeight}px`, 'left': (columnWidth * minColumnKey) + 'px'});
 
                 columns[minColumnKey] = minColumnHeight + pinHeight + gutter;
             });
 
-            $("ul.pins").css({ "height": Math.max.apply(Math, columns) + "px" });
+            $pins.css({ 'height': Math.max.apply(Math, columns) + 'px' });
 
         }
 
         // check for completion of initial load to set css transitions for pins
-        if(isActive === false){
-            $("ul.pins li").addClass("active");
+        if(!isActive){
+            $pin.addClass('active');
             setTimeout(function(){
-                $("ul.pins li").addClass("trans");
+                $pin.addClass('trans');
             }, 300);
             isActive = true;
         }
